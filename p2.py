924875ecb443058875b71a416cf1fef0bee91f08
kanha1998 <35166541+kanha1998@users.noreply.github.com>
2018-07-06 15:05:23 +0530

    Add files via upload
    color segmentation

---
+++ p2.py
@@ -0,0 +1,18 @@
+
+import cv2
+import numpy as np  
+
+img = cv2.imread('lineimage.jpg')
+
+lower_color = np.array([110,100,100])
+upper_color = np.array([130,255,255])
+
+hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv,lower_color,upper_color)
+
+cv2.imshow('mask',mask)
+
+res = cv2.bitwise_and(img,img,mask = mask)
+cv2.imshow('res',res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
